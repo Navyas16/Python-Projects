9f09d6b145c01c32eb043e7b7b1a3fbfa7d7d3af
Navya S <50953229+Navyas16@users.noreply.github.com>
2024-02-29 18:03:57 +0530

    Add files via upload

---
+++ numberguess.py
@@ -0,0 +1,28 @@
+import random
+name = input("Your name: ")
+print("Hello " + name)
+lower_limit = int(input("Enter ur lower limit:"))
+upper_limit = int(input("Enter ur upper limit:"))
+
+number = random.randint(lower_limit,upper_limit)
+
+chances = 7
+
+print("You have only 7 chances")
+while chances > 0:
+    guess = int(input("Your guess: "))
+    if guess == number:
+        print("You have successfully guessed the number")
+        print("Congratulations !!")
+        print(f"The number is {number}")
+        break
+    elif guess > number:
+         print("its too high")  
+    else:
+        print("its too low")
+    chances-=1
+    
+if chances == 0:
+    print("Better luch next time")
+    print(f"The number is {number}")
+    
